397e84893124ffc033cf7e7b77a177fb9ba04588
zhimengluo@foxmail.com <944581651@qq.com>
2017-02-20 15:27:00 +0800

    5post

--- _site/js/css3-ani.js
+++ _site/js/css3-ani.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function(){
 
     $('#cardflip').click(function(){
@@ -46,52 +45,3 @@ $(function(){
             });
     });
 });
-=======
-$(function(){
-
-    $('#cardflip').click(function(){
-        $('#card1').toggleClass('flipped')
-    });
-
-    $('body').delegate('#cardflip1','click',function(){
-        $('#card2').toggleClass('flipped')
-    });
-
-    $('body').delegate('#cube-btn button','click',function(){
-        var cls = $(this).attr('data-class');
-        $('#cube').removeClass();
-        $('#cube').addClass(cls);
-    });
-
-    $('#car-pre').click(function(){
-        var deg = $('#carousel').attr('data-deg') || 0;
-        deg = parseInt(deg)+40;
-
-        var value = 'translateZ(-288px) rotateY('+deg+'deg)';
-
-        $('#carousel')
-            .attr('data-deg',deg)
-            .css({
-                '-webkit-transform':value
-                ,'-moz-transform':value
-                ,'-o-transform':value
-                ,'transform':value
-            });
-    });
-    $('#car-next').click(function(){
-        var deg = $('#carousel').attr('data-deg') || 0;
-        deg = parseInt(deg)-40;
-
-        var value = 'translateZ(-288px) rotateY('+deg+'deg)';
-
-        $('#carousel')
-            .attr('data-deg',deg)
-            .css({
-                '-webkit-transform':value
-                ,'-moz-transform':value
-                ,'-o-transform':value
-                ,'transform':value
-            });
-    });
-});
->>>>>>> 57503ef9b61404040ef46ddb3442296effdae3de
--- _site/js/prettify/lang-tex.js
+++ _site/js/prettify/lang-tex.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 PR.registerLangHandler(PR.createSimpleLexer([["pln",/^[\t\n\r \xa0]+/,null,"\t\n\r Â\xa0"],["com",/^%[^\n\r]*/,null,"%"]],[["kwd",/^\\[@-Za-z]+/],["kwd",/^\\./],["typ",/^[$&]/],["lit",/[+-]?(?:\.\d+|\d+(?:\.\d*)?)(cm|em|ex|in|pc|pt|bp|mm)/i],["pun",/^[()=[\]{}]+/]]),["latex","tex"]);
-=======
-PR.registerLangHandler(PR.createSimpleLexer([["pln",/^[\t\n\r \xa0]+/,null,"\t\n\r Â\xa0"],["com",/^%[^\n\r]*/,null,"%"]],[["kwd",/^\\[@-Za-z]+/],["kwd",/^\\./],["typ",/^[$&]/],["lit",/[+-]?(?:\.\d+|\d+(?:\.\d*)?)(cm|em|ex|in|pc|pt|bp|mm)/i],["pun",/^[()=[\]{}]+/]]),["latex","tex"]);
->>>>>>> 57503ef9b61404040ef46ddb3442296effdae3de
